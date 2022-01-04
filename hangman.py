15ef331d5e1cfffa1683edf406cfbe7bdcff3c27
Anwesh Shaw <83204279+AnweshShaw@users.noreply.github.com>
2022-01-05 00:33:15 +0530

    Add files via upload
    This is a Hangman game which I made in which we have to guess the letters in the words.

---
+++ hangman.py
@@ -0,0 +1,94 @@
+import random
+import os
+print("\n                                              .........HANGMAN GAME........")
+words_list = ["abruptly","absurd","abyss","affix","avenue","awkward","axiom"]
+selected_word=random.choice(words_list)
+word_length=len(selected_word)
+print("Word: ",end="")
+for i in range(word_length):
+    print("_ ",end="")
+display=[]
+lives=6
+# Clear Screen command in python
+def cls():
+    os.system('cls' if os.name == 'nt' else 'clear')
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+
+for i in range(word_length):
+    display += "_"
+end=False
+while not end:
+    guess = input("\nGuess a letter: ")
+    guess = guess.lower()
+    for i in range(word_length):
+        letter=selected_word[i]
+        if (guess==letter):
+            display[i]=letter
+    if guess not in selected_word:
+        lives -= 1
+        if lives == 0:
+            end_of_game = True
+            print("\nSorry,You lost the game.")
+            print(f"The word is {selected_word}")
+            break
+    print(f"\nWord: {display}")
+    if "_" not in display:
+        end=True
+        print("\nCongratulations!!!,You won the game.")
+    else:
+        print(stages[lives])
