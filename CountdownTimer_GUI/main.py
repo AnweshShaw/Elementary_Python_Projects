29c06e2537463fc5337ddbb8a41a4f95ed2fb931
Anwesh Shaw <83204279+AnweshShaw@users.noreply.github.com>
2022-03-05 13:05:36 +0530

    Update main.py
    You can now select the time from the spinbox and use it to set a timer to whatever you like.

--- CountdownTimer_GUI/main.py
+++ CountdownTimer_GUI/main.py
@@ -3,7 +3,7 @@ from tkinter import messagebox
 import math
 
 FONT_NAME = "Courier"
-MIN = 1
+# MIN = 1/4
 timer = None
 RED = "#e7305b"
 GREEN = "#006400"
@@ -12,12 +12,6 @@ GREEN = "#006400"
 def reset_timer():
     window.after_cancel(timer)
     canvas.itemconfig(timer_text, text="00:00")
-    checkmark.config(text="")
-
-
-# Timer functioning
-def start_timer():
-    countdown(MIN * 60)
     checkmark.config(text="")
 
 # Working of Countdown
@@ -34,25 +28,49 @@ def countdown(count):
         global timer
         timer = window.after(1000, countdown, count - 1)
     else:
-        messagebox.showinfo(title="Message",message="Time is up!!!")
+        messagebox.showinfo(title="Message", message="Time is up!!!")
         checkmark.config(text="Time Up ✔", fg=GREEN)
         checkmark.place(x=110, y=300)
+
 
 # Creating UI
 window = Tk()
 window.title("Countdown Timer ")
-title_label = Label(text="Timer", fg=RED, font=(FONT_NAME,20, "bold"))
-# title_label.pack()
+title_label = Label(text="Timer", fg=RED, font=(FONT_NAME, 20, "bold"))
+title_label.place(x=100, y=0)
 
-canvas = Canvas(width=400, height=450,bg="cyan")
+canvas = Canvas(width=400, height=450, bg="cyan")
 clock_img = PhotoImage(file="image__3_-removebg-preview.png")
-canvas.create_image(200,200,image=clock_img)
+canvas.create_image(200, 200, image=clock_img)
 timer_text = canvas.create_text(200, 190, text="00:00", fill="red2", font=(FONT_NAME, 60, "bold"))
 canvas.pack()
 
-start_button = Button(text="Start ", highlightthickness=0, command=start_timer,bd=5)
+label = Label(text="MIN: ")
+label.place(x=40, y=28)
+label2 = Label(text="SEC: ")
+label2.place(x=180, y=28)
+start_button = Button(text="Start ", highlightthickness=0,bd=5)
 start_button.place(x=100, y=370)
-reset_button = Button(text="Reset", highlightthickness=0, command=reset_timer,bd=5)
+spin = Spinbox(window,from_=0,to_=60,width=10)
+spin.place(x=80, y=30)
+spin2 = Spinbox(window, from_=0, to_=60, width=10)
+spin2.place(x=220, y=30)
+
+def click():
+    MIN = int(spin.get())
+    SEC = int(spin2.get())
+
+    # Timer functioning
+    def start_timer():
+        countdown(int(MIN * 60)+SEC)
+        checkmark.config(text="")
+
+    start_button = Button(text="Start ", highlightthickness=0, command=start_timer, bd=5)
+    start_button.place(x=100, y=370)
+
+submit_button = Button(text="Submit", highlightthickness=0, command=click)
+submit_button.place(x=300, y=28)
+reset_button = Button(text="Reset", highlightthickness=0, command=reset_timer, bd=5)
 reset_button.place(x=250, y=370)
 checkmark = Label(text="Time Up ✔", fg=GREEN, font=("Algerian", 30, "bold"))
 
